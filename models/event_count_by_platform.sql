3f3e6510b560f04ae3cc56b41f1d5b38e9ec0529
joeldewater <joeldewater>
2023-11-27 13:49:54 +0000

    add count by platform

---
+++ models/event_count_by_platform.sql
@@ -0,0 +1,10 @@
+{{
+ config(materialized = 'table', file_format = 'delta', schema = "jdw")
+}}
+
+select 
+  platform,
+  count(event_id) as count_event_id
+from dbdemos.dbt_c360_bronze_events
+group by
+  platform
